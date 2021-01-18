843e777062ebe4bc4f51c0324f17f19ac226a679
pendragon <drth42011@yandex.ru>
2021-01-19 00:59:05 +0300

    First Commit

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.test
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var text:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        text = findViewById(R.id.textViewId)
+        text.setOnClickListener {
+            text.text = "hi"
+        }
     }
 }
